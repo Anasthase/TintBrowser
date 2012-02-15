b0566b85ab43086f055716e79f85654c7a1895dd
Anasthase <j.devauchelle@gmail.com>
2012-02-15 10:27:52 +0100

    Add license header to all files.

--- src/org/tint/ui/activities/BookmarksActivity.java
+++ src/org/tint/ui/activities/BookmarksActivity.java
@@ -1,3 +1,18 @@
+/*
+ * Tint Browser for Android
+ * 
+ * Copyright (C) 2012 - to infinity and beyond J. Devauchelle and contributors.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 3 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 package org.tint.ui.activities;
 
 import java.util.List;
--- src/org/tint/utils/UrlUtils.java
+++ src/org/tint/utils/UrlUtils.java
@@ -1,7 +1,7 @@
 /*
- * Zirco Browser for Android
+ * Tint Browser for Android
  * 
- * Copyright (C) 2010 J. Devauchelle and contributors.
+ * Copyright (C) 2012 - to infinity and beyond J. Devauchelle and contributors.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
